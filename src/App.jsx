e7517b0f2c7a13b636b9e69dd9d2aa5d1ea7b580
Camilo Figueroa <cigotete@gmail.com>
2023-12-05 11:30:23 -0500

    Added modal component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Button, PrimaryButton } from "./components/button";
+//import { Button, PrimaryButton } from "./components/button";
+import Modal from "./components/modal";
 import { GlobalStyles } from "./utils/index";
 
 export function App() {
 
   return (
     <>
-      <Button>Register</Button>
-      <PrimaryButton> Register</PrimaryButton>
+      {/* <Button>Click me!</Button> */}
+      {/* <PrimaryButton>Click me!</PrimaryButton> */}
+      <Modal />
 
       <GlobalStyles />
     </>
